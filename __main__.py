c97cdb8243606523ee394deb08fddd1b110d3871
AaronVickers <aaron_vickers@hotmail.co.uk>
2021-02-02 15:25:27 +0000

    Pause on minimise

--- __main__.py
+++ __main__.py
@@ -10,7 +10,24 @@ snake = Snake(board)
 interface = Interface(board, snake)
 
 def gameFunction():
+    paused = False
+    resumeCount = 0
+    
     while not interface.windowClosed:
+        if interface.state() != "normal":
+            paused = True
+            resumeCount = 0
+            
+            continue
+
+        if paused:
+            sleep(1)
+
+            resumeCount += 1
+            paused = resumeCount < 3
+
+            continue
+        
         snakeAlive = snake.move()
 
         if not snakeAlive:
